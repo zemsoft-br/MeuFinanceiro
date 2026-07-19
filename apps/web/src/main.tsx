3d1eb1592fffe298a01c157aa2dca06c8b9d37e5
zemsoft-br <vinicius.dezem@zemsoft.com.br>
2026-07-19 16:41:50 -0300

    fix(web): hide mobile controls on desktop
    Signed-off-by: Vinicius Eduardo Dezem <291430811+zemsoft-br@users.noreply.github.com>

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { registerServiceWorker } from './pwa'
 import './styles.css'
+import './responsive-shell.css'
 
 registerServiceWorker()
 
