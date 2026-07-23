14d415bb6d6b1836ce6fd21901501dde5fa986f9
zemsoft-br <vinicius.dezem@zemsoft.com.br>
2026-07-22 23:28:26 -0300

    test(demo): isolate bootstrap from demo endpoint
    Signed-off-by: Vinicius Eduardo Dezem <vinicius.dezem@zemsoft.com.br>

--- apps/app/test/app_bootstrap_test.dart
+++ apps/app/test/app_bootstrap_test.dart
@@ -3,10 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meufinanceiro_app/app/app.dart';
 import 'package:meufinanceiro_app/app/app_shell.dart';
+import 'package:meufinanceiro_app/core/demo/demo_status.dart';
 import 'package:meufinanceiro_app/core/health/api_health.dart';
 import 'package:meufinanceiro_app/features/home/home_screen.dart';
 import 'package:meufinanceiro_app/routing/app_router.dart';
 
+import 'support/fake_demo_transport.dart';
 import 'support/fake_health_transport.dart';
 
 void main() {
@@ -27,6 +29,10 @@ void main() {
           healthClockProvider.overrideWithValue(
             () => DateTime.utc(2026, 7, 20, 1),
           ),
+          demoStatusTransportProvider.overrideWithValue(disabledDemoTransport()),
+          demoStatusEndpointProvider.overrideWithValue(
+            Uri.parse('http://localhost/api/v1/demo/status'),
+          ),
           initialLocationProvider.overrideWithValue('/'),
         ],
         child: const MeuFinanceiroApp(),
@@ -37,6 +43,7 @@ void main() {
     expect(find.byKey(HomeScreen.titleKey), findsOneWidget);
     expect(find.byKey(AppShell.desktopSidebarKey), findsOneWidget);
     expect(find.byKey(AppShell.mobileNavigationKey), findsNothing);
+    expect(find.byKey(AppShell.demoNoticeKey), findsNothing);
     expect(find.text('Operacional'), findsWidgets);
     expect(transport.callCount, 1);
     expect(tester.takeException(), isNull);
